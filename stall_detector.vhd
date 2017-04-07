d9f547f85370bc6c03f15a8e923aed4fa7160025
3ammor <o.3ammor@gmail.com>
2017-04-07 20:15:11 +0200

    el sabak 4

--- stall_detector.vhd
+++ stall_detector.vhd
@@ -2,18 +2,25 @@ LIBRARY IEEE;
 USE IEEE.std_logic_1164.all;
 
 ENTITY pc_selector IS
-	PORT(	inc_pc, alu_pc, mem_pc : IN std_logic_vector(15 DOWNTO 0);
-		alu_br_taken, mem_br_taken, intR : IN std_logic;
-		output : OUT std_logic_vector(15 DOWNTO 0));
+	PORT(	rs, rt, rd, ID_rd, read_en : IN std_logic_vector(2 DOWNTO 0);
+		ID_load : IN std_logic;
+		output : OUT std_logic);
 END pc_selector;
 
 ARCHITECTURE a_pc_selector OF pc_selector IS
 	BEGIN
 
-		output <= "0000000000000001" when intR = '1'
-		else mem_pc when mem_br_taken = '1'
-		else alu_pc when alu_br_taken = '1'
-		else inc_pc;
+		output <= '1' when ID_load = '1' and ((read_en = "001" and rs = ID_rd) 
+							or (read_en = "010" and (rs = ID_rd or rt = ID_rd))
+							or (read_en = "011" and rd = ID_rd) 
+							or (read_en = "100" and (rs = ID_rd or rd = ID_rd)))
+		else '0';
 
 END a_pc_selector;
 
+-- read_en:
+--	000: no read
+--	001: rs
+--	010: rs & rt
+--	011: rd
+--	100: rs & rd
