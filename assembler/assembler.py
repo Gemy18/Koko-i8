7dfd190da0764840d1e6adca93a48c72f1676903
gemy18 <mahmoud.gemy18@gmail.com>
2017-04-08 13:44:17 +0200

    "hnakol a enharda"

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -1,4 +1,4 @@
-def abdullah(ins,fileout):
+def abdullah(ins):
     karem={"r0":"000"
            ,"r1":"001"
         , "r2": "010"
@@ -41,6 +41,8 @@ def abdullah(ins,fileout):
     ins=ins.replace(","," ")
     ins=' '.join(ins.split())
     lis=ins.split()
+    if (lis[0][0])==';':
+        return -1
     out=list("00000000000000000000000000000000")
     if lis[0]== "mov":
         out[5:8]=list(karem[lis[1]])
@@ -50,19 +52,65 @@ def abdullah(ins,fileout):
         out[8:11] = list(karem[lis[2]])
         out[11:14] =list(karem[lis[3]])
     elif lis[0] =="shr" or lis[0] =="shl" or lis[0] =="ldd" or lis[0] =="ldm" or lis[0] =="std"  :
-        out[16:32] = bin(int(lis[2]) % (1 << 16))[2:]
+        tmp = bin(int(lis[2]) % (1 << 16))[2:]
+        out[16:32]=list((16 - len(tmp)) * "0"+tmp)
         out[11:14] =list( karem[lis[1]])
     elif lis[0]=="nop" or lis[0]=="setc" or lis[0]=="clrc" or lis[0]=="ret" or lis[0]=="rti":
         pass
+
     else :
         out[11:14] =list( karem[lis[1]])
 
     out[0:5]= list(karem[lis[0]])
-    w = open(fileout, 'w')
-    w.write((''.join(out)))
-    w.write(('\n'))
+    return ''.join(out)
 
+def omer_magdy(filename,out1,out2):
+    f = open(filename, 'r')
+    dic1={}
+    dic2={}
+    for i in range(1024):
+        dic1[i]=  "0000000000000000"
+        dic2[i] = "0000000000000000"
+    data=0
+    instruction=0
+    new=-1
+    for line in f:
 
+        ins = line.lower()
+        ins = ins.replace(",", " ")
+        ins = ' '.join(ins.split())
+        lis = ins.split()
+        if len(lis)==0:
+            continue
+        if lis[0][0] == '-' or lis[0].isdigit():
 
+            tmp = bin(int(lis[0]) % (1 << 16))[2:]
+            temp = (16 - len(tmp)) * "0" + tmp
+            if new!= -1:
+                data=new
+                new=-1
+            dic1[data]=temp
+            data+=1
 
-abdullah( "mov r1 , r1","tempfile" )
+        elif lis[0][0] == '.':
+            new=int(lis[0][1:])
+        else :
+            if new != -1:
+                instruction = new
+                new = -1
+            s=abdullah(line)
+            if s==-1:
+                continue
+            dic2[instruction]=''.join(list(s)[16:])
+            dic2[instruction+1] = ''.join(list(s)[16:])
+            instruction+=2
+        w1 = open(out1, 'w')
+        w2 = open(out2, 'w')
+        for i in range(1024):
+            w1.write(str(i)+": "+str(dic1[i]))
+            w2.write(str(i) + ": "+str( dic2[i]))
+            w1.write(('\n'))
+            w2.write(('\n'))
+
+            # loop and print the mem
+omer_magdy("test.txt","data","ins")
